d41bb07d8d07dc39b9d3b88cee3a764cf9aaaf0a
ErwinSputra <erwinsputra757@gmail.com>
2023-04-19 07:22:33 +0700

    Getting data from server

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then(response => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "persons");
+
   return (
     <div className="App">
       <header className="App-header">
